bcc830ffa371b2bd1c11fc2c88d7494de55e36eb
Berni Mora <bgme171@gmail.com>
2017-10-22 18:31:55 -0600

    new: chg: Add files for scheduler

--- lab_2/main.cpp
+++ lab_2/main.cpp
@@ -4,6 +4,12 @@
 #include "Task.hpp"
 #include "LED.hpp"
 
+//#include <ti/devices/msp432p4xx/inc/msp.h>
+#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+#include <ti/grlib/grlib.h>
+#include "LcdDriver/Crystalfontz128x128_ST7735.h"
+#include <stdio.h>
+
 // ##########################
 // Global/Static declarations
 // ##########################
